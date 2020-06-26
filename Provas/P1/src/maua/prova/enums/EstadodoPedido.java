e87b032e3418cdf260b753b3f6773e8774d7f704
Daniel-xavier <danielughinix@hotmail.com>
2020-06-26 16:09:12 -0300

    Enums
    Criando os Enums

---
+++ Provas/P1/src/maua/prova/enums/EstadodoPedido.java
@@ -0,0 +1,5 @@
+package maua.prova.enums;
+
+public enum EstadodoPedido {
+    REALIZADO, PREPARACAO, SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO
+}
---
+++ Provas/P1/src/maua/prova/enums/FormadePagamento.java
@@ -0,0 +1,7 @@
+package maua.prova.enums;
+
+public enum FormadePagamento {
+
+    DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAOO;
+
+}
