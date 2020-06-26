88e7e1108456d9343b84a000d5eeb53764b1920d
Daniel-xavier <danielughinix@hotmail.com>
2020-06-26 13:47:00 -0300

    criacao do usuario

--- Provas/P1/src/maua/prova/pessoa/Usuario.java
+++ Provas/P1/src/maua/prova/pessoa/Usuario.java
@@ -1,6 +1,14 @@
 package maua.prova.pessoa;
 
+
 public class Usuario {
 
+    String nome, email, senha;
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = "123456";
+    }
     
 }
