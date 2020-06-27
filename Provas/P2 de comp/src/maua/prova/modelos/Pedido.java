249281597caa5e58dad606c145864d452d4a14c8
Daniel-xavier <danielughinix@hotmail.com>
2020-06-26 23:30:05 -0300

    Classe Pedido
    add o construtor

--- Provas/P2 de comp/src/maua/prova/modelos/Pedido.java
+++ Provas/P2 de comp/src/maua/prova/modelos/Pedido.java
@@ -40,6 +40,25 @@ import maua.prova.enums.*;
             this.descricao = descricao;
         }
 
-
+        public Pedido(String descricao, String valor, int formapagamento) {
+            switch (formapagamento) {
+                case 1:
+                    fpagamento = formadePagamento.CREDITO;
+                case 2:
+                    fpagamento = formadePagamento.DEBITO;
+                case 3:
+                    fpagamento = formadePagamento.DINHEIRO;
+                case 4:
+                    fpagamento = formadePagamento.VALE_ALIMENTACAO;
+                    break;
+                case 5:
+                    fpagamento = formadePagamento.VALE_REFEICAOO;
+                    break;
+            }
+            this.estado = estadodoPedido.REALIZADO;
+            this.Id = geradorId();
+            this.descricao = descricao;
+            this.valor = valor;
+        }
 
     }
