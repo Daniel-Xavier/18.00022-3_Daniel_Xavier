03f8a1e6bc7b4d6f1d4ef53564fe0874bb7db552
Daniel Xavier <54321539+Daniel-Xavier@users.noreply.github.com>
2020-12-01 18:37:25 -0300

    Add logo do Ow

--- Provas/P2/overwatch/lib/telas/my_home_page.dart
+++ Provas/P2/overwatch/lib/telas/my_home_page.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  final String title;
 
   const MyHomePage({Key key, this.title = 'Home'}) : super(key: key);
+   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final controlador = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold()
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              width: 300,
+              height:300,
+              child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/1200px-Overwatch_circle_logo.svg.png")
+            ),
+            
+
+        ],),),
+
+    )
   }
 }
