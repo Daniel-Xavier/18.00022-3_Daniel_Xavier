70399bb45f409cfcff6b48fe93f600407a41e775
Daniel Xavier <54321539+Daniel-Xavier@users.noreply.github.com>
2020-12-01 18:31:21 -0300

    Alterando a main pra iniciar o app e setar as rotas

--- Provas/P2/overwatch/lib/main.dart
+++ Provas/P2/overwatch/lib/main.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:overwatch/telas/my_home_page.dart';
+import 'package:overwatch/telas/overwatch_page.dart';
 
 void main() {
-  runApp(MyApp());
-}
+  runApp(
+    MaterialApp(
+      initialRoute: 'HomePage',
+      //initialRoute: 'OverwatchPage',
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      routes: {
+        'HomePage': (context) => MyHomePage(),
+        'OverwatchPage': (context) => OverwatchPage(),
+      },
+    ),
+  );
 }
