17ed745d0a30f7855a784bc31274f3e199050854
Daniel Xavier <54321539+Daniel-Xavier@users.noreply.github.com>
2020-12-01 18:28:26 -0300

    Criando a segunda tela

---
+++ Provas/P2/overwatch/lib/telas/overwatch_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class OverwatchPage extends StatefulWidget {
+  
+  _OverwatchPageState createState() => _OverwatchPageState();
+}
+
+class _OverwatchPageState extends State<OverwatchPage> {
+  TextEditingController controlador;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold()
+  }
+}
